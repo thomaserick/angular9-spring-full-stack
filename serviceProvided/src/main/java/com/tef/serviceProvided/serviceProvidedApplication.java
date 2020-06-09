b26884152696d6be06b49d4b7f2edaf4394ecd14
Thomas Erick <thominhaserick@gmail.com>
2020-06-08 21:39:46 -0300

    Start Project SpringBoot

---
+++ serviceProvided/src/main/java/com/tef/serviceProvided/serviceProvidedApplication.java
@@ -0,0 +1,12 @@
+package com.tef.serviceProvided;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class serviceProvidedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(serviceProvidedApplication.class, args);
+    }
+}
