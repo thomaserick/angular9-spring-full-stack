cb5c9b314caa4c236a828388814d8902c853ede1
Thomas Erick <thomas.oliveira@neomind.com.br>
2020-06-18 21:26:39 -0300

    Create component footer

--- serviceProvidedApp/src/app/template/template.module.ts
+++ serviceProvidedApp/src/app/template/template.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent, SidebarComponent],
+  declarations: [NavbarComponent, SidebarComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [NavbarComponent, SidebarComponent],
+  exports: [NavbarComponent, SidebarComponent, FooterComponent],
 })
 export class TemplateModule {}
