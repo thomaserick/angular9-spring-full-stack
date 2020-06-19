31b40da4e9dee5a8fb26d136d7c8fe6510233396
Thomas Erick <thomas.oliveira@neomind.com.br>
2020-06-18 21:10:43 -0300

    Create component navbar

--- serviceProvidedApp/src/app/app.module.ts
+++ serviceProvidedApp/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateModule } from './template/template.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, TemplateModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
