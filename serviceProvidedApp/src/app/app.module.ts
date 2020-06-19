b827a707c843e655edab23eb2cff686148e975fe
Thomas Erick <thomas.oliveira@neomind.com.br>
2020-06-18 21:29:45 -0300

    Create component Home

--- serviceProvidedApp/src/app/app.module.ts
+++ serviceProvidedApp/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, TemplateModule],
   providers: [],
   bootstrap: [AppComponent],
