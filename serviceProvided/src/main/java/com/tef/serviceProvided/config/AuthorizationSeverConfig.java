e8cc795befb9a26e8000158d5a0bf96bbd405f24
Thomas Erick <thominhaserick@gmail.com>
2020-07-13 20:39:41 -0300

    OAuth2

--- serviceProvided/src/main/java/com/tef/serviceProvided/config/AuthorizationSeverConfig.java
+++ serviceProvided/src/main/java/com/tef/serviceProvided/config/AuthorizationSeverConfig.java
@@ -2,6 +2,7 @@ package com.tef.serviceProvided.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +13,8 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -20,16 +23,26 @@ public class AuthorizationSeverConfig extends AuthorizationServerConfigurerAdapt
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Value("${security.jwt.signing-key}")
+    private String signingKey;
+
     @Bean
-    public TokenStore tokenStore()
-    {
-        return new InMemoryTokenStore();
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey(signingKey);
+        return tokenConverter;
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-      endpoints.tokenStore(tokenStore())
-              .authenticationManager(authenticationManager);
+        endpoints.tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter())
+                .authenticationManager(authenticationManager);
     }
 
     @Override
@@ -39,7 +52,7 @@ public class AuthorizationSeverConfig extends AuthorizationServerConfigurerAdapt
                 .inMemory()
                 .withClient("my-angular-app")
                 .secret("@321")
-                .scopes("read","write")   //O que vai fazer a aplicação
+                .scopes("read", "write")   //O que vai fazer a aplicação
                 .authorizedGrantTypes("password")
                 .accessTokenValiditySeconds(3600);  //1h vai dorar o dia
     }
